96165e54ea018735aa74e871aac09e9b9159b9f7
Samwel Benjamin Sitta <samwelbenjaminsitta@gmail.com>
2024-04-03 11:50:30 +0300

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
 import { Platform } from "./Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publishers";
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genre[];
+  publishers: Publisher[];
   slug: string;
   description_raw: string;
   background_image: string;
